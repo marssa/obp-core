6d8f022902827b380ade7088a66575a58b98798d
Robert Jaremczak <rjaremczak@gmail.com>
2014-04-08 17:57:06 +0200

    fixed readout class

--- src/main/java/org/obp/Readout.java
+++ src/main/java/org/obp/Readout.java
@@ -91,4 +91,9 @@ public class Readout {
     public long getUpdateTime() {
         return updateTime;
     }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
 }
